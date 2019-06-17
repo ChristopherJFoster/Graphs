25a1a5eb252897f7238a630688c4ccb8b8a120bc
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-17 11:13:25 -0400

    graph.py: Implemented DFT, including check for valid starting_vertex. Commented extensively on both BFT and DFT.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -32,29 +32,31 @@ class Graph:
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-
+        # Check for a valid starting vertex
         try:
             self.vertices[starting_vertex]
         except KeyError:
-            return f'The vertex you supplied ({starting_vertex}) does not exist in the graph.'
-
+            return f'The starting vertex you supplied ({starting_vertex}) does not exist in the graph.'
+        # Set up traversal record, queue, and color record for visited vertices
         traversal = []
         q = Queue()
         visited = {i: 'white' for i in self.vertices}
-
+        # Seed the queue with the starting vertex and color it grey
         q.enqueue(starting_vertex)
         visited[starting_vertex] = 'grey'
-
+        # Once the queue is empty, we're done. NOTE: This implementation will only traverse the graph component of which the starting vertex is a member.
         while q.size() > 0:
+            # unvisited = the set of valid neighbors minus the set of already visited vertices
             unvisited = self.vertices[
                 q.queue[0]] - set(k for k, v in visited.items() if v != 'white')
-            if len(unvisited) > 0:
-                for i in unvisited:
-                    q.enqueue(i)
-                    visited[i] = 'grey'
+            # Put each unvisited neighbor vertex in the queue and color it grey.
+            for i in unvisited:
+                q.enqueue(i)
+                visited[i] = 'grey'
+            # Now we're finished with the current vertex. Put it in the traversal record, then remove it from the queue and color it black.
             traversal.append(q.queue[0])
             visited[q.dequeue()] = 'black'
-
+        # Return the traversal record
         return traversal
 
     def dft(self, starting_vertex):
@@ -62,7 +64,35 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Check for a valid starting vertex
+        try:
+            self.vertices[starting_vertex]
+        except KeyError:
+            return f'The starting vertex you supplied ({starting_vertex}) does not exist in the graph.'
+        # Set up traversal record, stack, and color record for visited vertices
+        traversal = []
+        s = Stack()
+        visited = {i: 'white' for i in self.vertices}
+        # Seed the stack with the starting vertex and color it grey
+        s.push(starting_vertex)
+        visited[starting_vertex] = 'grey'
+        # Once the stack is empty, we're done. NOTE: This implementation will only traverse the graph component of which the starting vertex is a member.
+        while s.size() > 0:
+            # Since the stack is LIFO, we note the index of the current vertex.
+            current = len(s.stack) - 1
+            # unvisited = the set of valid neighbors minus the set of already visited vertices
+            unvisited = self.vertices[
+                s.stack[len(s.stack) - 1]] - set(k for k, v in visited.items() if v != 'white')
+            # Put each unvisited neighbor vertex in the stack and color it grey.
+            for i in unvisited:
+                s.push(i)
+                visited[i] = 'grey'
+            # If the current vertex is still on top of the stack, then it has no unvisited neighbors, and therefore has reached the full depth. We add it to the front of the traversal record (since the vertices are completed in reverse order), then remove it from the stack and color it black.
+            if current == len(s.stack) - 1:
+                traversal.insert(0, s.stack[len(s.stack) - 1])
+                visited[s.pop()] = 'black'
+        # Return the traversal record
+        return traversal
 
     def dft_recursive(self, starting_vertex):
         """
@@ -117,15 +147,6 @@ if __name__ == '__main__':
     print('Vertices and edges: ', graph.vertices)
 
     '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    # print('DFT: ', graph.dft(1))
-
-    '''
     Valid BFT paths:
         1, 2, 3, 4, 5, 6, 7
         1, 2, 3, 4, 5, 7, 6
@@ -141,6 +162,15 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 5, 6
     '''
     print('BFT: ', graph.bft(1))
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    print('DFT: ', graph.dft(1))
 
     '''
     Valid DFT recursive paths:
