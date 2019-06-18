019132906db9e10d458acae5ee185e7c6c95b63b
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-18 16:31:58 -0400

    ancestor.py: complete. test_ancestor.py: passes.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,16 +1,4 @@
-example = [(1, 3),
-           (2, 3),
-           (3, 6),
-           (5, 6),
-           (5, 7),
-           (4, 5),
-           (4, 8),
-           (8, 9),
-           (11, 8),
-           (10, 1)]
-
-
-def earliest_ancestor(id, data, ancs=None):
+def earliest_ancestor(data, id, ancs=None):
     # Create an empty list for ancestors. Each ancestor will itself be a list that begins with the starting id and ends with the ancestor, with each descendent (if any) in between. It is prepopulated with the starting id.
     if ancs == None:
         ancs = []
@@ -19,7 +7,6 @@ def earliest_ancestor(id, data, ancs=None):
     for i in range(len(data)):
         if data[i][1] == id:
             # Add parent to ancs:
-            print(ancs)
             temp_anc = []
             for j in range(len(ancs)):
                 if ancs[j][-1] == id:
@@ -27,13 +14,16 @@ def earliest_ancestor(id, data, ancs=None):
             temp_anc.append(data[i][0])
             ancs.append(temp_anc)
             # Recursively call earliest_ancestor on each parent
-            earliest_ancestor(data[i][0], data, ancs)
+            earliest_ancestor(data, data[i][0], ancs)
 
     # Base case is when there are no parents for a given id
 
     # return the ancestor from ancs with the longest list (lowest id if tied). Not yet sure how to check for this in a recursive function.
-    earliest = sorted(ancs, key=lambda anc: -len(anc))
-    return earliest
-
-
-print(earliest_ancestor(6, example))
+    if len(ancs) == 1:
+        return -1
+    else:
+        sorted_by_earliest = sorted(ancs, key=lambda anc: -len(anc))
+        earliest = [x[-1] for x in sorted_by_earliest if len(
+            x) == len(sorted_by_earliest[0])]
+        # earliest =
+        return min(earliest)
