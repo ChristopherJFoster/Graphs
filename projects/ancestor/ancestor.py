6c1f0964ba5961e10f0d7e4fffa861e153799457
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-18 16:53:32 -0400

    ancestor.py: Cleaned up code a bit.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,5 +1,5 @@
 def earliest_ancestor(data, id, ancs=None):
-    # Create an empty list for ancestors. Each ancestor will itself be a list that begins with the starting id and ends with the ancestor, with each descendent (if any) in between. It is prepopulated with the starting id.
+    # Create an empty list for ancestors. Each ancestor will itself be a list that begins with the starting id and ends with the ancestor, with each descendent (if any) in between. It is prepopulated with the provided id.
     if ancs == None:
         ancs = []
         ancs.append([id])
@@ -12,18 +12,11 @@ def earliest_ancestor(data, id, ancs=None):
                 if ancs[j][-1] == id:
                     temp_anc = list(ancs[j])
             temp_anc.append(data[i][0])
-            ancs.append(temp_anc)
+            ancs.insert(0, temp_anc) if len(temp_anc) > len(
+                ancs[0]) else ancs.append(temp_anc)
             # Recursively call earliest_ancestor on each parent
             earliest_ancestor(data, data[i][0], ancs)
 
-    # Base case is when there are no parents for a given id
-
-    # return the ancestor from ancs with the longest list (lowest id if tied). Not yet sure how to check for this in a recursive function.
-    if len(ancs) == 1:
-        return -1
-    else:
-        sorted_by_earliest = sorted(ancs, key=lambda anc: -len(anc))
-        earliest = [x[-1] for x in sorted_by_earliest if len(
-            x) == len(sorted_by_earliest[0])]
-        # earliest =
-        return min(earliest)
+    # Return the earliest ancestor from ancs (the ancestor represented by the longest list). Return lowest id if tied. If len(ancs) is not greater than 1, that means the provided id has no parents: return -1.
+    return min([x[-1] for x in ancs if len(
+        x) == len(ancs[0])]) if len(ancs) > 1 else -1
