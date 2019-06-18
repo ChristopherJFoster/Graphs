0a5c4a3ab1a36c612a76e548dd3098be152d3a07
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-18 14:16:23 -0400

    ancestor.py: Wrote a basic plan in comments.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -10,8 +10,18 @@ example = [[1, 3],
            [10, 1]]
 
 
-def earliest_ancestor(id, data):
-    pass
+def earl_anc(id, data, ancs=None):
+    # Create an empty list for ancestors. Each ancestor will itself be a list that begins with the starting id and ends with the ancestor, with each descendent (if any) in between. It is prepopulated with the starting id.
+    if ancs == None:
+        ancs = [[id]]
+        earl_anc(ancs[-1][-1], data, ancs)
+    # Find parents of the current id by looking through data for any list that ends with the id. Add any parents to ancestors.
+
+    # Recursively call earl_and on each parent
+
+    # Base case is when there are no parents for a given id
+
+    # return the ancestor from ancs with the longest list (lowest id if tied). Not yet sure how to check for this in a recursive function.
 
 
-print(earliest_ancestor(6, example))
+print(earl_anc(6, example))
